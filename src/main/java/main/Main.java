502b16ea3332ec449c6fa58133fe7579560daae2
schegolevanv <ariwenn15@gmail.com>
2020-12-15 14:43:35 +0300

    add PostConstruct method for creating directory "upload"

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,6 @@
 package main;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -7,14 +9,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.util.TimeZone;
 
+@Slf4j
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableScheduling
 @PropertySource("classpath:constants.yml")
 @EntityScan(basePackages = {"main/model"})
 public class Main {
+
+    @Value("${storage.location}")
+    private String location;
 
     public static void main(String[] args) {
 
@@ -24,5 +31,9 @@ public class Main {
     @PostConstruct
     public void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        File upload = new File(location);
+        if (upload.mkdir())
+            log.info("Directory {} was successfully created", location);
+        else log.info("The {} directory already exist", location);
     }
 }
